8f1823ced91fcb670364ba2edf6f5877b90b1865
sly1061101 <sly1061101@gmail.com>
2017-12-02 11:16:22 -0600

    Initial

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  438-CSMA-simulator
+//
+//  Created by Liuyi Shi on 12/2/17.
+//  Copyright © 2017 Liuyi Shi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(){
+    printf("Enjoy 438 MP4!!!\n");
+    return 0;
+}
